7ecda9bb877772ba372ae732a6e76d7ac688cb29
buzzon <buzzondev@gmail.com>
2020-07-01 15:52:45 +0500

    Object level permissions

--- quickstart/views.py
+++ quickstart/views.py
@@ -1,13 +1,30 @@
 from quickstart.models import Snippet
-from quickstart.serializers import SnippetSerializer
-from rest_framework import generics
+from quickstart.permissions import IsOwnerOrReadOnly
+from quickstart.serializers import SnippetSerializer, UserSerializer
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
