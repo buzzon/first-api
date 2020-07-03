ab638dfd15e4267f6ba401bec0683a3d3d797e2a
buzzon <buzzondev@gmail.com>
2020-07-03 12:36:34 +0500

    ViewSets & Routers

--- quickstart/urls.py
+++ quickstart/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path, include
-from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 from quickstart import views
 
-urlpatterns = [
-    path('', views.api_root),
-    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
-    path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
-    path('auth/', include('rest_framework.urls')),
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
 
+urlpatterns = [
+    path('', include(router.urls)),
+    path('auth/', include('rest_framework.urls')),
 ]
